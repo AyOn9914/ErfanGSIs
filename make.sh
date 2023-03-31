891fba7d9063d7207bb9f7f290dd59bea108efbf
Akari <barryofc11@gmail.com>
2023-03-31 07:18:59 -0300

    Update make.sh
    added support for a13 and a14

--- make.sh
+++ make.sh
@@ -139,6 +139,8 @@ case "$sourcever" in
     *"10"*) flag=true ;;
     *"11"*) flag=true ;;
     *"12"*) flag=true ;;
+    *"13"*) flag=true ;;
+    *"14"*) flag=true ;;
 esac
 if [ "$flag" == "false" ]; then
     echo "$sourcever is not supported"
