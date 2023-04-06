ee9d68e4bfd97c5301a029fc6b2c67a3b088ad3b
Akari Nyan 梓川 <barryofc11@gmail.com>
2023-04-06 03:57:27 -0300

    update fix
    added -y for apt

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
        sudo pacman -S --needed unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc xz python-pip brotli lz4 gawk libmpack aria2
        #aur=rar
     else
-       sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python3-pip brotli liblz4-tool gawk aria2 openjdk-17-jdk
+       sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python3-pip brotli liblz4-tool gawk aria2 openjdk-17-jdk -y
     fi
     pip install -r requirements.txt
 elif [[ "$OSTYPE" == "darwin"* ]]; then
